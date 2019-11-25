707ed7c75820eec8b0ad26706866a9bfab50df47
Rishabh Raj <ga27woy@mytum.de>
2019-11-25 01:17:33 +0100

    ransac

--- Homework1.1/code/hw1_task2_skeleton.m
+++ Homework1.1/code/hw1_task2_skeleton.m
@@ -50,7 +50,7 @@ sift_matches=cell(num_files,1);
 
 % Default threshold for SIFT keypoints matching: 1.5 
 % When taking higher value, match is only recognized if similarity is very high
-threshold_ubcmatch = 1.5; 
+threshold_ubcmatch = 2.0; % default 1.5 - increase to filter points, atlternative - SCORES
 
 % for i=1:num_files
 %     fprintf('Calculating and matching sift features for image: %d \n', i)
@@ -103,12 +103,83 @@ threshold_ransac = 4;
 
 for i = 1:num_files
     fprintf('Running PnP+RANSAC for image: %d \n', i)
+    random_points_count = 4;
+    % Get all the keypoint matches between Image and Model 
+    % Give me all the 2D (x,y) of center of frame for the matched keypoints
+    sift_matches_image = keypoints{i}(1:2, sift_matches{i}(1, :));
+    % Give me all the 3D (x,y,z) of the corresponding matched keypoints
+    sift_matches_model = model.coord3d(sift_matches{i}(2, :), :);
+        
+    % Prepare sift_matches_model for projection onto the image --
+    % Homogeneous Cordinates
+    sift_matches_model(:, 4) = 1;
    
 %     TODO: Implement the RANSAC algorithm here
+    best_inliers_set_size = 3;
+    for itr = 1:ransac_iterations
+        % give me 4 random interger valued indexes from the total sift matches
+        sampled_points = randperm(size(sift_matches{i}, 2), random_points_count);
+        % give me the actual matched keypoints; matched_keypoints(1,:) is the
+        % keypoint number in Image which matched matched_keypoints(2,:) in the
+        % model
+        matched_keypoints = sift_matches{i}(:, sampled_points);
+        index_keypoints_image = matched_keypoints(1, :);
+        index_keypoints_model = matched_keypoints(2, :);
 
+        % Get the center of the frame of the keypoints of Image
+        image_x_y = keypoints{i}(1:2, index_keypoints_image);
+        
+        % Similarly get the corresponding world locations from the Sift Model
+        world_x_y = model.coord3d(index_keypoints_model, :);
+        % Estimate the pose using PnP
+        try
+            [cam_in_world_orientations_local, cam_in_world_locations_local] = estimateWorldCameraPose(image_x_y', world_x_y, camera_params, 'MaxReprojectionError', 1000);
+            % Sometimes this configurations results in no inliers
+        catch ME
+            continue
+        end
+        
+        % Convert camera pose to extrinsics
+        [rotationMatrix, translationVector] = cameraPoseToExtrinsics(cam_in_world_orientations_local, cam_in_world_locations_local);
+        
+        % Compute camera projection matrix
+        camMatrix = cameraMatrix(camera_params, rotationMatrix, translationVector);
+        
+        % Project onto image
+        camera_cordinates = sift_matches_model * camMatrix;
+        
+        % get rid of z, project to z=0
+        camera_cordinates_z = camera_cordinates(:, 3);
+        image_cordinates = camera_cordinates(:, 1:2) ./ camera_cordinates_z;
+        
+        % Get the reprojection error
+        reprojection_error = sift_matches_image - image_cordinates'; % use pdist2, already tried better than vecnorm
+        direction_vector = vecnorm(reprojection_error, 2, 1);
+        
+        % Get inliers indexes
+        inliers_index = find(direction_vector < threshold_ransac);
+        
+        % Update inliers
+        if (numel(inliers_index) > best_inliers_set_size)
+            best_inliers_set{i} = inliers_index;
+            best_inliers_set_size = numel(best_inliers_set{i});
+        end
+    end
+    
+    % Restimate the pose
+    [cam_in_world_orientations(:,:,i),cam_in_world_locations(:,:,i)] = estimateWorldCameraPose(sift_matches_image(:, best_inliers_set{i})', sift_matches_model(best_inliers_set{i}, 1:3), camera_params, ...
+        'MaxReprojectionError', 1000);
     
 end
 
+% Result Improvement:
+% TODO-
+% 1. Is dense way theoratically sound? - Not clear if the method handles the
+% shift!
+% 2. Better and precise corners selection
+% 3. More ransac iterations. 9 wrong for 50 itr
+% 4. Play with reprojection error - 1000 default, try reducing
+% 5. Maybe as last resort numIter and Confidence too
 
 
 %% Visualize inliers and the bounding box
