e97c01fe99a62f54141aff2909646017fe96f1d3
Rishabh Raj <ga27woy@mytum.de>
2019-12-29 12:23:16 +0100

    HW2 : task1 done ( can improve code style )

---
+++ Homework 2/Homework2/src/task1/task1.cpp
@@ -0,0 +1,28 @@
+
+#include <opencv2/opencv.hpp>
+
+#include "../../include/HOGDescriptor.h"
+
+
+
+
+int main(){
+    cv::Mat im = cv::imread("../data/task1/obj1000.jpg");
+    /*cv::imshow("image", im);
+    cv::waitKey(0);*/
+	//Fill Code here
+
+    /*
+    	* Create instance of HOGDescriptor and initialize
+    	* Compute HOG descriptors
+    	* visualize
+    */
+
+    std::vector<float> descriptors;
+
+    HOGDescriptor hogDescriptor;
+    hogDescriptor.detectHOGDescriptor(im, descriptors, cv::Size(128, 128), true);
+    //hogDescriptor.visualizeHOG(im, descriptors, hogDescriptor.getHog_detector(), 8);
+
+    return 0;
+}
