03b77216ac03ca438b335bcb79bdbe4a53a1d3f9
Rishabh Raj <ga27woy@mytum.de>
2020-01-26 19:04:53 +0100

    Task 3: model and loss-- tested

--- Homework 3/src/loss.py
+++ Homework 3/src/loss.py
@@ -6,16 +6,16 @@ class TripletAndPairLoss(nn.Module):
     def __init__(self, batch_size: int, margin: float = 0.01, pair_loss_weight_factor: float = 1):
         super(TripletAndPairLoss, self).__init__()
         self.batch_size = batch_size
-        self.margin = margin
+        self.margin = torch.tensor(margin)
         self.pair_loss_weight_factor = pair_loss_weight_factor
 
     def forward(self, x):
         anchor = x[0:self.batch_size * 3:3]
         positive = x[1:self.batch_size * 3:3]
         negative = x[2:self.batch_size * 3:3]
-        positive_distance = torch.sum((anchor - positive).pow(2), dim=1)
-        negative_distance = torch.sum((anchor - negative).pow(2), dim=1)
-        triplet_loss = torch.max(0.0, (1 - (negative_distance) / (positive_distance + self.margin)))
+        positive_distance: torch.Tensor = torch.sum((anchor - positive).pow(2), dim=1)
+        negative_distance: torch.Tensor = torch.sum((anchor - negative).pow(2), dim=1)
+        triplet_loss = torch.max(torch.tensor(0.0), (torch.tensor(1.0) - negative_distance / (positive_distance + self.margin)))
         pair_loss = positive_distance
 
         triplet_loss = torch.sum(triplet_loss)
--- Homework 3/src/main.py
+++ Homework 3/src/main.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
+from models import DescriptorNetwork
+from loss import TripletAndPairLoss
+
 import matplotlib.pyplot as plt
 from datasets import TripletDataset
 import numpy as np
@@ -13,18 +16,11 @@ import numpy as np
 # # plt.imshow(data[5000][0])
 # # plt.show()
 # print('done')
-x = torch.randn(size=(8, 3, 64, 64))
-c1 = nn.Conv2d(3, 16, 8, stride=1, padding=0)
-m1 = nn.MaxPool2d(2, stride=2)
-c2 = nn.Conv2d(16, 7, 5, stride=1, padding=0)
-m2 = nn.MaxPool2d(2, stride=2)
-l1 = nn.Linear(1008, 256)
-l2 = nn.Linear(256, 16)
-out = c1(x)
-out = m1(out)
-out = c2(out)
-out = m2(out)
-out = out.view(out.size(0), -1)
-out = l2(l1(out))
+x = torch.randn(size=(9, 3, 64, 64))
+m = DescriptorNetwork()
+l = TripletAndPairLoss(batch_size=9)
+out = m(x)
+loss = l(out)
 print(out.size())
+print(loss)
 
