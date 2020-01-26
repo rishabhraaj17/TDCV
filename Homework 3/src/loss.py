81c19f7de4f4c938e22e8e1481f1fc612ddf5d74
Rishabh Raj <ga27woy@mytum.de>
2020-01-26 17:59:23 +0100

    Task 3: model and loss

---
+++ Homework 3/src/loss.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class TripletAndPairLoss(nn.Module):
+    def __init__(self, batch_size: int, margin: float = 0.01, pair_loss_weight_factor: float = 1):
+        super(TripletAndPairLoss, self).__init__()
+        self.batch_size = batch_size
+        self.margin = margin
+        self.pair_loss_weight_factor = pair_loss_weight_factor
+
+    def forward(self, x):
+        anchor = x[0:self.batch_size * 3:3]
+        positive = x[1:self.batch_size * 3:3]
+        negative = x[2:self.batch_size * 3:3]
+        positive_distance = torch.sum((anchor - positive).pow(2), dim=1)
+        negative_distance = torch.sum((anchor - negative).pow(2), dim=1)
+        triplet_loss = torch.max(0.0, (1 - (negative_distance) / (positive_distance + self.margin)))
+        pair_loss = positive_distance
+
+        triplet_loss = torch.sum(triplet_loss)
+        pair_loss = self.pair_loss_weight_factor * torch.sum(pair_loss)
+        return triplet_loss, pair_loss
--- Homework 3/src/main.py
+++ Homework 3/src/main.py
@@ -1,14 +1,30 @@
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
 from datasets import TripletDataset
 import numpy as np
 
-data = TripletDataset(data_dir='../dataset/', train=True, online=False)
-data_loader = DataLoader(dataset=data, batch_size=8, shuffle=True, num_workers=0)
-for d in data_loader:
-    print(d)
-# plt.imshow(data[5000][0])
-# plt.show()
-print('done')
+# data = TripletDataset(data_dir='../dataset/', train=True, online=False)
+# data_loader = DataLoader(dataset=data, batch_size=8, shuffle=True, num_workers=0)
+# for d in data_loader:
+#     print(d)
+# # plt.imshow(data[5000][0])
+# # plt.show()
+# print('done')
+x = torch.randn(size=(8, 3, 64, 64))
+c1 = nn.Conv2d(3, 16, 8, stride=1, padding=0)
+m1 = nn.MaxPool2d(2, stride=2)
+c2 = nn.Conv2d(16, 7, 5, stride=1, padding=0)
+m2 = nn.MaxPool2d(2, stride=2)
+l1 = nn.Linear(1008, 256)
+l2 = nn.Linear(256, 16)
+out = c1(x)
+out = m1(out)
+out = c2(out)
+out = m2(out)
+out = out.view(out.size(0), -1)
+out = l2(l1(out))
+print(out.size())
+
