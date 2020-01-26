e7975958ace5ca4f59a04ef5bf829f38ea33f6d9
Rishabh Raj <ga27woy@mytum.de>
2020-01-26 16:00:43 +0100

    Task 3: dataset done -- look for same pose issue if results are bad

--- Homework 3/src/datasets.py
+++ Homework 3/src/datasets.py
@@ -2,6 +2,71 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.datasets import VisionDataset
+from utils.data_utils import del_theta_quaternion, get_train_mean_and_std, get_normalized_datasets
+
+
+# Notes: Anchor pose and puller pose is same, maybe coarse and fine has same pose, for 1 sample looks like it is the case
+
+class DataSample(object):
+    def __init__(self, image: torch.Tensor, label: int, pose: torch.Tensor):
+        super(DataSample, self).__init__()
+        self.image = image
+        self.label = label
+        self.pose = pose
+
+
+class Triplet(object):
+    def __init__(self, anchor: DataSample, puller: DataSample, pusher: DataSample):
+        super(Triplet, self).__init__()
+        self.anchor = anchor
+        self.puller = puller
+        self.pusher = pusher
+
+
+class TripletDataset(Dataset):
+    def __init__(self, data_dir: str, train: bool = True, online: bool = False):
+        super(TripletDataset, self).__init__()
+        self.data_dir = data_dir
+        if online:
+            classes = ("ape", "benchvise", "cam", "cat", "duck")
+            m, std = get_train_mean_and_std(data_dir, classes)
+            db, train, test = get_normalized_datasets(data_dir, classes, m, std)
+            if train:
+                self.dataset = train
+            else:
+                self.dataset = test
+            self.db = db
+        else:
+            if train:
+                self.dataset = torch.load(data_dir + 'S_train.pt')
+            else:
+                self.dataset = torch.load(data_dir + 'S_test.pt')
+            self.db = torch.load(data_dir + 'S_db.pt')
+
+    def get_puller(self, sample: list):
+        puller = None
+        min_distance = 50000
+        for i in range(len(self.db)):
+            if sample[1] == self.db[i][1]:
+                distance = del_theta_quaternion(q1=sample[2:6], q2=self.db[i][2:6])
+                if distance < min_distance:
+                    min_distance = distance
+                    puller = self.db[i]
+        return puller
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, item):
+        anchor = self.dataset[item]
+        puller = self.get_puller(anchor)
+        pusher_idx = torch.randint(len(self.db), (1,)).item()
+        pusher = self.db[pusher_idx]
+        anchor = DataSample(image=torch.from_numpy(anchor[0]), label=anchor[1], pose=torch.tensor(anchor[2:]))
+        puller = DataSample(image=torch.from_numpy(puller[0]), label=puller[1], pose=torch.tensor(puller[2:]))
+        pusher = DataSample(image=torch.from_numpy(pusher[0]), label=pusher[1], pose=torch.tensor(pusher[2:]))
+        return Triplet(anchor, puller, pusher)
+        # return anchor, puller, pusher
 
 
 class DummyDataset(VisionDataset):
